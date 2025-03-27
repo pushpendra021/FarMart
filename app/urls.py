336d7de8635933ff69a37455488a1557c64f1129
pushpendra021 <pushpendrabais021@gmail.com>
2025-03-27 17:46:05 +0530

    its done

---
+++ app/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm,MyPasswordChangeForm
+urlpatterns = [
+    
+    path('',views.ProductView.as_view(), name="home"),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    
+
+    path('harvesting_tools/', views.harvesting_tools, name='harvesting_tools'),
+    path('harvesting_tools/<slug:data>', views.harvesting_tools, name='harvesting_toolsdata'),
+
+    path('tractor_parts/', views.tractor_parts, name='tractor_parts'),
+    path('tractor_parts/<slug:data>', views.tractor_parts, name='tractor_partsdata'),
+
+
+    path('irrigation_systems/', views.irrigation_systems, name='irrigation_systems'),
+    path('irrigation_systems/<slug:data>', views.irrigation_systems, name='irrigation_systemsdata'),
+
+
+
+
+    # path('login/', views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
+
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
+
+
+
+    # path('registration/', views.customerregistration, name='customerregistration'),
+
+    path('checkout/', views.checkout, name='checkout'),
+    path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration')
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ app/views.py
@@ -0,0 +1,121 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Customer,Product,Cart, OrderPlaced
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
+
+class ProductView(View):
+    def get(self, request):
+        tractor_parts = Product.objects.filter(category='TPA')
+        irrigation_systems = Product.objects.filter(category='IS')
+        harvesting_tools = Product.objects.filter(category='HT')
+    
+        seeds_saplings = Product.objects.filter(category='SS')
+        fertilizers_boosters = Product.objects.filter(category='FGB')
+        organic_products = Product.objects.filter(category='OFP')
+        # You can now return these in context if rendering a template
+        context = {
+            'tractor_parts': tractor_parts,
+            'irrigation_systems': irrigation_systems,
+            'harvesting_tools': harvesting_tools,
+          
+         
+            'seeds_saplings': seeds_saplings,
+            'fertilizers_boosters': fertilizers_boosters,
+            'organic_products': organic_products,
+        }
+        return render(request, 'app/home.html', context)
+
+# def home(request):
+#  return render(request, 'app/home.html')
+
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product': product})
+
+
+
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+
+
+def harvesting_tools(request,data=None):
+ if data==None:
+   harvesting=Product.objects.filter(category='HT')
+ elif data=="LocalForge" or data=="GreenPluck":
+   harvesting=Product.objects.filter(category='HT').filter(brand=data)
+ elif data == 'below':
+    harvesting = Product.objects.filter(category='HT').filter(discounted_price__lt=10000)
+ elif data == 'above':
+    harvesting= Product.objects.filter(category='HT').filter(discounted_price__gt=10000)
+ 
+ return render(request, 'app/harvesting_tools.html',{'harvesting':harvesting})
+
+#navigation first section done
+def tractor_parts(request,data=None):
+ if data==None:
+   tractor=Product.objects.filter(category='TPA')
+ elif data=="AgroLink" or data=="CropSharp" or data=="AgriParts":
+   tractor=Product.objects.filter(category='TPA').filter(brand=data) 
+ elif data == 'below':
+    tractor = Product.objects.filter(category='TPA').filter(discounted_price__lt=10000)
+ elif data == 'above':
+   tractor = Product.objects.filter(category='TPA').filter(discounted_price__gt=10000)
+ 
+ return render(request, 'app/tractor_parts.html',{'tractor':tractor})
+
+def irrigation_systems(request,data=None):
+ if data==None:
+   irrigation=Product.objects.filter(category='IS')
+ elif data=="model1" or data=="model2" or data=="model3":
+   irrigation=Product.objects.filter(category='IS').filter(brand=data) 
+ elif data == 'below':
+    irrigation= Product.objects.filter(category='IS').filter(discounted_price__lt=10000)
+ elif data == 'above':
+   irrigation = Product.objects.filter(category='IS').filter(discounted_price__gt=10000)
+ 
+ return render(request, 'app/irrigation_systems.html',{'irrigation':irrigation})
+
+
+#navigation 2nd section done
+
+# def login(request):
+#  return render(request, 'app/login.html')
+
+# def customerregistration(request):
+#  return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Registration successful!')
+            
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
